096893d68e4739d4232f8a101e5ba3c74d957022
usaginoshippo <86910445+usaginoshippo@users.noreply.github.com>
2021-10-24 13:52:15 +0900

    Issue6の再修正　曜日の表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,11 +36,11 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = Date.today.wday
-      if wday_num+x >= 7
-        wday_num = wday_num+x - 7 
+      if wday_num >= 7
+        wday_num = wday_num - 7 
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: wdays[wday_num]}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: wdays[wday_num+x]}
       @week_days.push(days)
     end
 
